964ba11fcd7af055e8bbe5985747b67ee7c3f6af
tungnk181120 <tung.nk183661@gmail.com>
2022-05-06 16:06:02 +0700

    mit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 2!
+          Hello World 3!
         </p>
         <a
           className="App-link"
