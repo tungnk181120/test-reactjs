9b99dc165d178c436e39aa5782753c37858689d1
Nguyen Kim Tung <72078935+tungnk181120@users.noreply.github.com>
2022-06-09 13:21:26 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 3!
+          Hello World 4!
         </p>
         <a
           className="App-link"
